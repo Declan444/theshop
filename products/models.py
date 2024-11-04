e4f2975ae6d7ea8a86d7ee244ee6de6af5bfc6f1
Declan <laresearchlabs@gmail.com>
2024-11-04 20:03:06 +0000

    add whitenoise for staticfiles to be deployed to heroku

--- products/models.py
+++ products/models.py
@@ -2,32 +2,34 @@ from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
     """
-    Product Categories 
+    Product Categories
     """
+
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = "Categories"
 
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254,
-                                     null=True, blank=True)
-    
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
     def __str__(self):
         return self.name
-    
+
     def get_friendly_name(self):
         return self.friendly_name
-    
+
 
 class Products(models.Model):
-    """
-    
-    """
+    """ """
+
     class Meta:
-        verbose_name_plural = 'Products'
-        
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+        verbose_name_plural = "Products"
+
+    category = models.ForeignKey(
+        "Category", null=True, blank=True, on_delete=models.SET_NULL
+    )
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
@@ -35,7 +37,9 @@ class Products(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    is_weekly_bargain = models.BooleanField(default=False, help_text='Mark this product as a weekly bargain')
+    is_weekly_bargain = models.BooleanField(
+        default=False, help_text="Mark this product as a weekly bargain"
+    )
 
     def __str__(self):
         return self.name
