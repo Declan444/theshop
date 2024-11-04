c9f296dc03edb4afdfa6bf44a39b970fa45cc68f
Declan <laresearchlabs@gmail.com>
2024-11-04 23:07:51 +0000

    Added add to shopping bag view and functionality

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_shopping_bag, name='view_shopping_bag'),
-    
+    path('add/<item_id>/', views.add_to_shopping_bag, name='add_to_shopping_bag'),
 ]
