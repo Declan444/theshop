696f99c2a59b21cae1ae401568d037e4453bf952
Declan <laresearchlabs@gmail.com>
2024-11-24 21:25:28 +0000

    Setup email confirmation

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1", "equestrian-online-a728dbfb5f33.herokuapp.com"]
 
 
@@ -183,6 +183,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'equestrianshop@example.com'
 
 
 # Default primary key field type
